2cc32d8de6e9cb08de09a92d5456646ab0e7691b
DESKTOP-BJSHKLG\eric <jini7652@gmail.com>
2021-07-23 18:25:48 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+
+
+## Project Dependencies
+
+* DEVELOPER TOOLS
+- Lombok: 상용구 코드를 줄이는데 도움이 되는 Java 주석 라이브러ㅣ
+- Spring Boot DevTools: 향상된 개발 환경을 ㅟ해 빠른 애플리케이션 재시작, LiveRoad 및 구성을 제공
+
+* WEB
+- Spring Web WEB: 스프링 MVC를 사용하여 RESTful을 포함한 웹을 구축한다. Apache Tomcat을 기본 내장형 컨테이너로 사용.
+
+* TEMPLATE ENGINES
+- Thymeleaf: 웹 및 독립 실행형 환경 모두를 위한 최신 서버측 Java 템플릿 엔진. Html을 브라우저 및 정적 프로토타입으로 올바르게 표시할 수 있다.
+  
+* SECURITY
+- Spring Security: Spring 애플리케이션을 위한 사용자 지정이 가능한 높은 인증 및 엑세스 제어 프레임워크
+
+* SQL
+- Spring Data JPA: SpringData 및 Hibernate를 사용하여 Java Persistance API를 사용하여 SQL 저장소에 데이터를 유지한다.
+
+* I/O
+- Validation: Bean 유효성 검사기를 사용한 검증.
---
+++ src/main/java/com/eric/website/domain/BaseEntity.java
@@ -0,0 +1,29 @@
+package com.eric.website.domain;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Getter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @CreatedDate
+    @Column(updatable = false)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime createdDDate;
+
+    @LastModifiedBy
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yy-MM-dd", timezone = "Asia/Seoul")
+    private LocalDateTime modifieddate;
+}
